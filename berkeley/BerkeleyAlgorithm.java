23de60997a362e7290116d7b3fad07e2d2e6b2ba
Joel <jchinop@unsa.edu.pe>
2025-05-01 16:28:23 -0500

    Agregar implementación del Algoritmo de Berkeley y archivos de configuración del proyecto

---
+++ berkeley/BerkeleyAlgorithm.java
@@ -0,0 +1,17 @@
+package berkeley;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementación del Algoritmo de Berkeley para sincronización de relojes
+ *
+ * Este algoritmo selecciona un servidor como coordinador que recolecta las
+ * diferencias de tiempo de todos los demás servidores, calcula un tiempo promedio,
+ * y envía los ajustes necesarios a cada servidor.
+ */
+public class BerkeleyAlgorithm {
+    public static void main(String[] args) {
+
+    }
+}
