d0b47cf8d15479d9ff02d3b357c09d2dc8a96b95
Joel <jchinop@unsa.edu.pe>
2025-05-01 16:31:27 -0500

    Agregar constructor en SynchronizationStatistics para inicializar la lista de relojes

--- berkeley/SynchronizationStatistics.java
+++ berkeley/SynchronizationStatistics.java
@@ -5,4 +5,10 @@ import java.util.List;
  * Clase para calcular y mostrar estadísticas sobre la sincronización
  */
 public class SynchronizationStatistics {
+    private final List<Clock> clocks;
+
+    public SynchronizationStatistics(List<Clock> clocks) {
+        this.clocks = clocks;
+    }
+
 }
